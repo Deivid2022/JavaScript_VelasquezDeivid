11c3e1720347279a31d092b94817650fae94dc44
Deivid <deividvelasquez122@gmail.com>
2024-04-18 06:40:55 -0500

    feat: :rocket: Creacion de la tarea
    Creacion de la tarea

--- Dia4/script.js
+++ Dia4/script.js
@@ -15,4 +15,43 @@ const Usuario2 = new Usuario('Brayan', 'Tennis');
 Usuario1.saludar();
 Usuario2.saludar();
 
+class Coche {
+    constructor(marca, modelo, año, color) {
+      this.marca = marca;
+      this.modelo = modelo;
+      this.año = año;
+      this.color = color;
+    }
+  
+    conducir() {
+      console.log("El coche está en movimiento.");
+    }
+  }
+  
+const coche = new Coche("Toyota", "Corolla", 2022, "rojo");
+coche.conducir()
+
+class Libro {
+constructor(titulo, autor, genero, paginas) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.genero = genero;
+    this.paginas = paginas;
+}
+
+    leer() {
+        console.log("Estoy leyendo el libro.");
+    }
+}
+
+const libro = new Libro("Cien años de soledad", "Gabriel García Márquez", "realismo mágico", 432);
+libro.leer()
+
+const numero = 42;
+
+function suma(a, b) {
+    return a + b;
+  }
+  
+  
 
