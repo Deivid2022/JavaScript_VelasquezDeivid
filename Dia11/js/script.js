0a3a39a5c361431e61a01e4177e78c516f654bc1
Deivid <deividvelasquez122@gmail.com>
2024-04-26 18:26:29 -0500

    feat: :sparkles: Terminacion de dia 11
    Se termino correctamente el dia 11

--- Dia11/js/script.js
+++ Dia11/js/script.js
@@ -1,36 +1,64 @@
-function getPokemonInfo() {
-            const pokemonInput = document.getElementById('pokemonInput').value.toLowerCase();
-            const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonInput}`;
+function getPokemonInfo(pokemonNameOrId) {
+    const apiUrl = `https://pokeapi.co/api/v2/pokemon/${pokemonNameOrId}`;
 
-            fetch(apiUrl)
-                .then(response => {
-                    if (!response.ok) {
-                        throw new Error('Pagina fuera de servicio');
-                    }
-                    return response.json();
-                })
-                .then(data => {
-                    const pokemonid = data.id;
-                    const pokemonName = data.name;
-                    const pokemonAnimatedSpriteUrl = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+    fetch(apiUrl)
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Página fuera de servicio');
+            }
+            return response.json();
+        })
+        .then(data => {
+            const pokemonid = data.id;
+            const pokemonName = capitalizeFirstLetter(data.name);
+            const pokemonAnimatedSpriteUrl = data.sprites.versions['generation-v']['black-white'].animated.front_default;
+            const imagen = `./storage/img/pikachu.png`;
 
-                    document.getElementById('pokemonid').textContent = `${pokemonid}`
-                    document.getElementById('pokemonName').textContent = `${pokemonName}`;
-                    document.getElementById('pokemonImage').src = pokemonAnimatedSpriteUrl;
-                })
-                .catch(error => {
-                    console.error('Ocurrio un problema con tu función: ', error);
-                });
-        }
+            document.getElementById('pokemonid').textContent = `${pokemonid} - ${pokemonName}`;
+            if (pokemonAnimatedSpriteUrl !== null) {
+                document.getElementById('pokemonImage').src = pokemonAnimatedSpriteUrl;
+            } else {
+                document.getElementById('pokemonImage').src = imagen;
+            }
+        })
+        .catch(error => {
+            console.error('Ocurrió un problema con tu función: ', error);
+        });
+}
 
-// Obtener el input
-var input = document.getElementById("pokemonInput");
+function capitalizeFirstLetter(string) {
+    return string.charAt(0).toUpperCase() + string.slice(1);
+}
+const pokemonInput = document.getElementById('pokemonInput');
+const clearBtn = document.getElementById('clearBtn');
+const prevBtn = document.querySelector('.button1');
+const nextBtn = document.querySelector('.button2');
 
-// Agregar un event listener para detectar cuando se presiona Enter
-input.addEventListener("keyup", function(event) {
-    // Verificar si la tecla presionada es Enter (código 13)
-    if (event.keyCode === 13) {
-        // Llamar a la función getPokemonInfo()
-        getPokemonInfo();
-    }
+
+clearBtn.addEventListener('click', function() {
+  pokemonInput.value = '';
+});
+
+
+pokemonInput.addEventListener('keyup', function(event) {
+  if (event.key === 'Enter') {
+    getPokemonInfo(pokemonInput.value.toLowerCase());
+    pokemonInput.value = '';
+  }
+});
+
+
+prevBtn.addEventListener('click', function() {
+  const currentPokemonId = parseInt(document.getElementById('pokemonid').textContent);
+  const prevPokemonId = currentPokemonId - 1;
+  if (prevPokemonId > 0) {
+    getPokemonInfo(prevPokemonId);
+  }
+});
+
+
+nextBtn.addEventListener('click', function() {
+  const currentPokemonId = parseInt(document.getElementById('pokemonid').textContent);
+  const nextPokemonId = currentPokemonId + 1;
+  getPokemonInfo(nextPokemonId);
 });
